43b440cd1a169b278a34bcbf6246a8650ce41ca8
Nikhil Kr. Nishad <nikhilnishad279@gmail.com>
2022-06-05 23:59:02 +0530

    hello world router added with all files and configurations required
    Signed-off-by: Nikhil Kr. Nishad <nikhilnishad279@gmail.com>

---
+++ movies-review-service/src/main/java/com/reactivespring/router/ReviewRouter.java
@@ -0,0 +1,21 @@
+package com.reactivespring.router;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+public class ReviewRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> reviewRoute(){
+        return route()
+                .GET("/v1/hello",request ->
+                    ServerResponse.ok().bodyValue("hello world!")
+                )
+                .build();
+    }
+}
