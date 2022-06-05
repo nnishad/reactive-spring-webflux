984af80af06e7c20c9538a6b8842eb604c0e01e2
Nikhil Kr. Nishad <nikhilnishad279@gmail.com>
2022-06-05 15:11:43 +0530

    Flux, mono and stream controller added with unit testcases

---
+++ movies-info-service/src/test/java/unit/com/reactivespring/controller/FluxAndMonoControllerTest.java
@@ -0,0 +1,76 @@
+package com.reactivespring.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.test.StepVerifier;
+
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+@WebFluxTest(controllers = FluxAndMonoController.class)
+@AutoConfigureWebTestClient
+class FluxAndMonoControllerTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    void fluxEndpoint() {
+        webTestClient.get()
+                .uri("/flux")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBodyList(Integer.class)
+                .hasSize(3);
+    }
+
+    @Test
+    void fluxEndpoint_approach2() {
+        var flux= webTestClient.get()
+                .uri("/flux")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(flux)
+                .expectNext(1,2,3)
+                .verifyComplete();
+    }
+
+    @Test
+    void fluxEndpoint_approach3() {
+        webTestClient.get()
+                .uri("/flux")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBodyList(Integer.class)
+                .consumeWith(listEntityExchangeResult -> {
+                    var responseBody=listEntityExchangeResult.getResponseBody();
+                    assert Objects.requireNonNull(responseBody).size()==3;
+                });
+    }
+
+    @Test
+    void streamEndpoint_approach2() {
+        var flux= webTestClient.get()
+                .uri("/stream")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .returnResult(Long.class)
+                .getResponseBody();
+
+        StepVerifier.create(flux)
+                .expectNext(0L,1L,2L,3L)
+                .thenCancel()
+                .verify();
+    }
+
+}
